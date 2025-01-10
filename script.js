9ea07a7b3b814a0449b5efc6b8f1152b25494164
vipul5543 <129362602+vipul5543@users.noreply.github.com>
2025-01-10 14:46:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function stringChop(str, size) {
   // your code here
+ if (size <= 0) {
+    throw new Error("Chunk size must be greater than 0");
+  }
+
+  let result = [];
+  let index = 0;
+
+  // Loop to chop the string into chunks
+  while (index < str.length) {
+    result.push(str.substring(index, index + size));
+    index += size;
+  }
+
+  return result;
+	
 }
 
 // Do not change the code below
